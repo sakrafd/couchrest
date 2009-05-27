44c5ae6276062ecaa5b083057eb67c989c3a9871
Peter Gumeson <gumeson@gmail.com>
2009-05-28 04:24:25 +0800

    Added attributes= to casted model and extended doc
    Signed-off-by: Arnaud Berthomier <oz@cyprio.net>

--- lib/couchrest/more/extended_document.rb
+++ lib/couchrest/more/extended_document.rb
@@ -125,6 +125,7 @@ module CouchRest
         self.send("#{k}=",v)
       end
     end
+    alias :attributes= :update_attributes_without_saving
 
     # Takes a hash as argument, and applies the values by using writer methods
     # for each key. Raises a NoMethodError if the corresponding methods are
