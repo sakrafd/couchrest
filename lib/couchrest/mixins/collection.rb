db276001c30ce259ea34fd71044a5da31c570e16
Arnaud Berthomier <arnaud@cyprio.net>
2009-07-20 15:44:35 +0200

    meh

--- lib/couchrest/mixins/collection.rb
+++ lib/couchrest/mixins/collection.rb
@@ -207,7 +207,7 @@ module CouchRest
           if @container_class.nil?
             results
           else
-            results['rows'].collect { |row| @container_class.new(row['doc']) } rescue => []
+            results['rows'].collect { |row| @container_class.new(row['doc']) } unless results['rows'].nil?
           end
         end
 
